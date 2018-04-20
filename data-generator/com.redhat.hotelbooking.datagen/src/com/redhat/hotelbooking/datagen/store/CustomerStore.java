d97101f77593b0d6997af4888571cf148cf32ff1
Daniel Florian <dflorian@redhat.com>
2018-04-20 11:34:12 -0500

    Changes to data generator and added documentation - the readme as been modified to talk about the data model and data generation - added status to the reservation table - moved starting table row ID constants to the settings object

--- data-generator/com.redhat.hotelbooking.datagen/src/com/redhat/hotelbooking/datagen/store/CustomerStore.java
+++ data-generator/com.redhat.hotelbooking.datagen/src/com/redhat/hotelbooking/datagen/store/CustomerStore.java
@@ -30,13 +30,12 @@ public final class CustomerStore implements DomainStore {
     public enum Column {
 
         ADDRESS_LINE_1,
-        CITY,
+        CITY_ID,
         EMAIL,
         ID,
         MEMBER_SINCE,
         NAME,
         PASSWORD,
-        POSTAL_CODE,
         REWARDS_ID;
 
         public String toCreateStatement() {
@@ -47,8 +46,8 @@ public final class CustomerStore implements DomainStore {
                 case ADDRESS_LINE_1:
                     builder.append( "CHARACTER VARYING(256) NOT NULL" );
                     break;
-                case CITY:
-                    builder.append( "CHARACTER VARYING(256) NOT NULL" );
+                case CITY_ID:
+                    builder.append( "INTEGER NOT NULL" );
                     break;
                 case EMAIL:
                     builder.append( "CHARACTER VARYING(256) NOT NULL" );
@@ -65,9 +64,6 @@ public final class CustomerStore implements DomainStore {
                 case PASSWORD:
                     builder.append( "CHARACTER VARYING(256) NOT NULL" );
                     break;
-                case POSTAL_CODE:
-                    builder.append( "CHARACTER VARYING(256) NOT NULL" );
-                    break;
                 case REWARDS_ID:
                     builder.append( "CHARACTER VARYING(256) NOT NULL" );
                     break;
@@ -81,29 +77,33 @@ public final class CustomerStore implements DomainStore {
     }
 
     public static final String TABLE_NAME = "CUSTOMER";
+    public static final String CITY_FK_NAME = "CITY_FK";
 
     private static final String COLUMNS = DomainStore.toColumnsStatement( Column.ID.name(),
                                                                           Column.NAME.name(),
-                                                                          Column.PASSWORD.name(),
                                                                           Column.EMAIL.name(),
-                                                                          Column.REWARDS_ID.name(),
-                                                                          Column.MEMBER_SINCE.name(),
+                                                                          Column.PASSWORD.name(),
                                                                           Column.ADDRESS_LINE_1.name(),
-                                                                          Column.CITY.name(),
-                                                                          Column.POSTAL_CODE.name() );
+                                                                          Column.CITY_ID.name(),
+                                                                          Column.MEMBER_SINCE.name(),
+                                                                          Column.REWARDS_ID.name() );
 
-    private static final String CREATE_TABLE_STMT = "CREATE TABLE " + DomainStore.addQuotes( TABLE_NAME ) + " (\n"
-                                                    + "\t" + Column.ID.toCreateStatement() + ",\n"
-                                                    + "\t" + Column.NAME.toCreateStatement() + ",\n"
-                                                    + "\t" + Column.PASSWORD.toCreateStatement() + ",\n"
-                                                    + "\t" + Column.EMAIL.toCreateStatement() + ",\n"
-                                                    + "\t" + Column.REWARDS_ID.toCreateStatement() + ",\n"
-                                                    + "\t" + Column.MEMBER_SINCE.toCreateStatement() + ",\n"
-                                                    + "\t" + Column.ADDRESS_LINE_1.toCreateStatement() + ",\n"
-                                                    + "\t" + Column.CITY.toCreateStatement() + ",\n"
-                                                    + "\t" + Column.POSTAL_CODE.toCreateStatement() + ",\n"
-                                                    + "\tPRIMARY KEY ( " + DomainStore.addQuotes( Column.ID.name() ) + " )\n"
-                                                    + ");";
+    private static final String CREATE_TABLE_STMT
+        = "CREATE TABLE " + DomainStore.addQuotes( TABLE_NAME ) + " (\n"
+          + "\t" + Column.ID.toCreateStatement() + ",\n"
+          + "\t" + Column.NAME.toCreateStatement() + ",\n"
+          + "\t" + Column.EMAIL.toCreateStatement() + ",\n"
+          + "\t" + Column.PASSWORD.toCreateStatement() + ",\n"
+          + "\t" + Column.ADDRESS_LINE_1.toCreateStatement() + ",\n"
+          + "\t" + Column.CITY_ID.toCreateStatement() + ",\n"
+          + "\t" + Column.MEMBER_SINCE.toCreateStatement() + ",\n"
+          + "\t" + Column.REWARDS_ID.toCreateStatement() + ",\n"
+          + "\tPRIMARY KEY ( " + DomainStore.addQuotes( Column.ID.name() ) + " ),\n"
+          + "\t" + DomainStore.createForeignKeyConstraint( CITY_FK_NAME,
+                                                           Column.CITY_ID.toString(),
+                                                           CityStore.TABLE_NAME,
+                                                           CityStore.Column.ID.toString() ) + "\n"
+          + ");";
 
     private static final String INSERT_STMT = "INSERT INTO "
                                               + DomainStore.addQuotes( TABLE_NAME )
@@ -129,13 +129,12 @@ public final class CustomerStore implements DomainStore {
             final String insertStmt = String.format( INSERT_STMT,
                                                      toDdl( customer.getId() ),
                                                      toDdl( customer.getName() ),
-                                                     toDdl( customer.getPassword() ),
                                                      toDdl( customer.getEmail() ),
-                                                     toDdl( customer.getRewardsId() ),
-                                                     toDdl( customer.getMemberSince() ),
+                                                     toDdl( customer.getPassword() ),
                                                      toDdl( customer.getAddressLine1() ),
-                                                     toDdl( customer.getCity() ),
-                                                     toDdl( customer.getPostalCode() ) );
+                                                     toDdl( customer.getCityId() ),
+                                                     toDdl( customer.getMemberSince() ),
+                                                     toDdl( customer.getRewardsId() ) );
             ddl.append( insertStmt ).append( '\n' );
         }
 
