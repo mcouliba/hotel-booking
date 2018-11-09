bcae31421f4f02fee47114d77d70da698eaa73d0
mcouliba <mcouliba@redhat.com>
2018-11-09 16:52:13 +0100

    Added a new MongoDB DataSources with Reward Information

---
+++ hotel-booking-new-sources/rewarddb/initRewardDB.js
@@ -0,0 +1,8 @@
+db.customers.drop();
+db.customers.insert([
+	  { "customer_id" : 407, "status" : "GOLD" }
+	, { "customer_id" : 408, "status" : "SILVER" }	
+	, { "customer_id" : 409, "status" : "GOLD" }
+	, { "customer_id" : 410, "status" : "BRONZE" }
+	, { "customer_id" : 411, "status" : "PLATINIUM" }
+]);
